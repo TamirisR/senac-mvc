3c878900ab05ab8cff415809f2ce51df7ed14d32
TamirisR <Tamis_rodrigues@hotmail.com>
2021-03-15 18:17:03 -0300

    aula + exercicio

--- app/Models/Vendas.php
+++ app/Models/Vendas.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Vendas extends Model
 {
@@ -12,4 +13,8 @@ class Vendas extends Model
     protected $fillable = ['id', 'cliente_id', 'funcionario_id', 'data_da_venda', 'valor'];
 
     protected $table = 'Vendas';
+
+    public function cliente(){
+        return $this->belongsTo(Clientes::class, 'cliente_id');
+    }
 }
