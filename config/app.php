fe5d90f1ed2318e225dd3d8667b8ee090d6352c9
TamirisR <Tamis_rodrigues@hotmail.com>
2021-04-16 22:15:43 -0300

    aula 16-04

--- config/app.php
+++ config/app.php
@@ -175,6 +175,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        //Provider para controle de perfil de usuario
+        Spatie\Permission\PermissionServiceProvider::class,
     ],
 
     /*
