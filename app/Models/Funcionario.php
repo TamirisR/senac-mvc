598470cbc9f28f5570a2795717e2e14b1ec01bb8
TamirisR <Tamis_rodrigues@hotmail.com>
2021-05-04 00:36:37 -0300

    aula dia 30

--- app/Models/Funcionario.php
+++ app/Models/Funcionario.php
@@ -8,7 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Funcionario extends Model
 {
     use HasFactory;
-    protected $fillable = [ 'id', 'nome', 'endereco', 'email', 'telefone'];
+
+    protected $fillable = ['id','nome','endereco','email','telefone'];
 
     protected $table = 'Funcionario';
+
+    public function funcionariovendas(){
+        return $this->hasMany( Vendas::class, 'funcionario_id');
+    }
 }
--- app/Models/Vendas.php
+++ app/Models/Vendas.php
@@ -4,17 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Vendas extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['id', 'cliente_id', 'funcionario_id', 'data_da_venda', 'valor'];
+    protected $fillable = ['id','cliente_id','funcionario_id','data_da_venda','valor'];
 
     protected $table = 'Vendas';
 
     public function cliente(){
-        return $this->belongsTo(Clientes::class, 'cliente_id');
+        return $this->belongsTo(Clientes::class,'cliente_id');
     }
 }
